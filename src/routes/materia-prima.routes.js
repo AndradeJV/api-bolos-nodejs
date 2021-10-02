bb0e713e413c0c478530b598b0685008f66226bb
Joao Andrade <joaovandrade30@gmail.com>
2021-10-02 02:03:55 -0300

    Issue #4
    - Iniciando manutenção das routes

--- src/routes/materia-prima.routes.js
+++ src/routes/materia-prima.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { db } = require('../models/materiaPrima');
 const router = express.Router();
 const MateriaPrima = require('../models/materiaPrima');
 
 
 router.get('/rawMaterials', async (req, res) => {
-
+  
   try {
     const materiaPrima = await MateriaPrima.find({ });
     res.json({ error: false, materiaPrima });
@@ -12,32 +13,35 @@ router.get('/rawMaterials', async (req, res) => {
   } catch (error) {
     res.json({ error: error, message: error.message });
   }
+
 });
 
-router.get('/rawMaterials/:name', async (req, res) => {
+router.get('/rawMaterials/:id', async (req, res) => {
 
   try {
-    const materiaPrima = await MateriaPrima.find({
-      name: req.params.name
-     });
+    const id = req.params.id;
+    const materiaPrima = await MateriaPrima.findById(id);
+
     res.json({ error: false, materiaPrima });
     
   } catch (error) {
     res.json({ error: error, message: error.message });
   }
+
 });
 
-router.get('/rawMaterials/:user', async (req, res) => {
+router.get('/rawMaterials/:name/req', async (req, res) => {
 
   try {
-    const materiaPrima = await MateriaPrima.find({
-      user: req.params.user
-     });
+    const name = req.params.name;
+    const materiaPrima = await MateriaPrima.findById(name);
+
     res.json({ error: false, materiaPrima });
     
   } catch (error) {
     res.json({ error: error, message: error.message });
   }
+
 });
 
 router.post('/rawMaterials', async (req, res) => {
@@ -50,6 +54,7 @@ router.post('/rawMaterials', async (req, res) => {
   } catch (error) {
     res.json({ error: true, message: error.message }); 
   }
+
 });
 
 router.put('/rawMaterials/:id/request', async (req, res) => {
